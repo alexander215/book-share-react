b5ae1108824cee982d76af093fcc051b701d81c6
Narek <narex111@gmail.com>
2019-08-26 18:18:28 -0700

    edit user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch, Link } from "react-router-dom"
+import { Route, Switch, Link, Redirect } from "react-router-dom"
 import { Button } from 'reactstrap';
 import Login from "./Login"
 import Register from './Register'
 import Profile from "./Profile"
 import NavBar from './Navbar'
+import EditProfile from "./EditProfile"
 
 import { async } from 'q';
 import Users from './Users';
@@ -75,6 +76,32 @@ class App extends Component {
     }
   }
 
+  // deleteClick = async (id, history) => {
+  //   console.log("hitting deleteClick")
+  //   try {
+  //     const userResponse = await fetch('http://localhost:8000/user/' + id, {
+  //       method: "DELETE",
+  //       credentials: "include",
+  //       // body: data,
+  //       // headers: {
+  //       //   'Content-Type': 'application/json'
+  //       // }
+  //     })
+  //     console.log("before parsed response")
+  //     // const parsedResponse = await userResponse.json()
+  //     // console.log(parsedResponse, '<-- parsedResponse in deleteClick ')
+
+  //     // router.push('/')
+  //     // return history.push(`/`)
+  //     return <Redirect to='/users' />
+
+  //     // return parsedResponse
+
+  //   } catch (err){
+  //     console.log(err)
+  //   }
+  // }
+
   userList = async(data) =>{
     console.log("hitting userList")
     try{
@@ -108,8 +135,9 @@ class App extends Component {
             <Route exact path ="/" component = {generic}></Route>
             <Route exact path = "/login" render={(props) => <Login {...props} logIn={this.login}/>}></Route>
             <Route exact path = "/register" render={(props) => <Register {...props} register={this.register}></Register>}></Route>
-            <Route exact path = "/profile/:index" render={(props) => <Profile {...props} userInfo={this.state}></Profile>}></Route>
+            <Route exact path = "/profile/:index" render={(props) => <Profile {...props} deleteClick={this.deleteClick} userInfo={this.state}></Profile>}></Route>
             <Route exact path= "/users" render={(props) => <Users {...props} userState={this.state} userList={this.userList}></Users>}></Route>
+            <Route exact path= "/:id/edit" render={(props) => <EditProfile {...props} userState={this.state}></EditProfile>}></Route>
             <Route component = {My404}/>
           </Switch>
         </div>
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react"
-import {Link} from "react-router-dom"
+import React, { Component } from "react"
+import { Link, Redirect } from "react-router-dom"
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { thisExpression } from "@babel/types";
+import EditProfile from "../EditProfile"
 
 class Profile extends Component{
     state={
@@ -20,13 +21,45 @@ class Profile extends Component{
         })
     }
 
+    deleteClick = async (id, history) => {
+        console.log("hitting deleteClick")
+        try {
+          const userResponse = await fetch('http://localhost:8000/user/' + id, {
+            method: "DELETE",
+            credentials: "include",
+            // body: data,
+            // headers: {
+            //   'Content-Type': 'application/json'
+            // }
+          })
+          console.log("before parsed response")
+          // const parsedResponse = await userResponse.json()
+          // console.log(parsedResponse, '<-- parsedResponse in deleteClick ')
+    
+          // router.push('/')
+          return history.push(`/`)
+        //   return <Redirect to='/users' />
+    
+          // return parsedResponse
+    
+        } catch (err){
+          console.log(err)
+        }
+      }
+
     render(){
         console.log(this.state, "state in profile")
         console.log(this.props, "props in profile")
         return(
             <div>
                 <div><h2>{this.state.username} is here!</h2></div>
-                <img src={`${process.env.REACT_APP_BACKEND_URL}/profile_pics/` + this.state.photo}/>
+
+                <div><img src={`${process.env.REACT_APP_BACKEND_URL}/profile_pics/` + this.state.photo}/></div>
+                <br/>
+                <div>
+                    <Button color="warning" onClick={ () => this.props.history.push(`/${this.state.id}/edit`)}>Edit</Button>{' '}
+                    <Button color="danger" onClick={() => { this.deleteClick(this.state.id) }}>Delete</Button>{' '}
+                </div>
             </div>
         )
     }
